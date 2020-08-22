90142febb0ca2f3ea46831f90fd211c10f6bf75c
Peng <penghuang0508@gmail.com>
2020-08-22 00:20:04 -0700

    First commit

---
+++ quotes.js
@@ -0,0 +1,30 @@
+export const QUOTES = [
+  {
+    author: 'STEPHEN HAWKING',
+    quote:
+      "I don't think the human race will survive the next thousand years, unless we spread into space. There are too many accidents that can befall life on a single planet. But I'm an optimist. We will reach out to the stars.",
+    origin: 'The Daily Telegraph',
+  },
+  {
+    author: 'ROBERT A. HEINLEIN',
+    quote:
+      'The stars will never be won by little minds; we must be big as space itself.',
+    origin: 'Double Star',
+  },
+  {
+    author: 'ZERO 7',
+    quote: 'We are the light that travels into space.',
+    origin: '"Today"',
+  },
+  {
+    author: 'CHRISTA MCAULIFFE',
+    quote:
+      "Space is for everybody. It's not just for a few people in science or math, or for a select group of astronauts. That's our new frontier out there, and it's everybody's business to know about space.",
+    origin: 'December 6, 1985',
+  },
+  {
+    author: 'CARL SAGAN',
+    quote: 'All civilizations become either spacefaring or extinct.',
+    origin: ' Pale Blue Dot',
+  },
+];
---
+++ script.js
@@ -0,0 +1,42 @@
+import { QUOTES } from './quotes.js';
+
+/////
+// Display random quote from local quote selections
+/////
+var quotation = document.querySelector('blockquote.quotation');
+
+const randomQuote = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+
+quotation.innerHTML = `"${randomQuote.quote}" <p>${randomQuote.author}, ${randomQuote.origin}</p>`;
+
+// Fetch from data from NASA
+
+// Fetch using NASA APOD (Astronomy Picture of the Day) API
+var apodArticle = document.querySelector('#astronomyPictureOfTheDay');
+console.log(apodArticle);
+
+// TODO USE DEMO KEY
+// https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+
+fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then((res) => res.json())
+  .then((data) => {
+    const APOD_MARKUP = `
+      <h5>Astronomy Picture of the Day</h5>
+      <img
+        src=${data.url}
+        srcset="${data.url},
+                ${data.hdurl}"
+        alt=${data.title}
+      />
+      <h3>${data.title}</h3>
+      <p>${data.explanation}</p>
+      `;
+
+    document.querySelector('.firstArticle').classList.toggle('firstArticle');
+    apodArticle.classList.add('firstArticle');
+
+    apodArticle.innerHTML = APOD_MARKUP;
+
+    return;
+  });
